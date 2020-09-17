a0e5655888dab20e59cc674deb3bfb44dbb94280
Earshad <ahamed.a@technotackle.com>
2020-09-17 15:53:09 +0530

    Updates

---
+++ precache-manifest.3a3644240a7756acf5ba064933d8221a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bcaa15ad61cce02174a8eccb5992904",
+    "url": "/agoraDemo/index.html"
+  },
+  {
+    "revision": "68f1df482b34d6ca4f2f",
+    "url": "/agoraDemo/static/css/2.da82b403.chunk.css"
+  },
+  {
+    "revision": "2a4fce5ae2260c0fee84",
+    "url": "/agoraDemo/static/css/main.13aae214.chunk.css"
+  },
+  {
+    "revision": "68f1df482b34d6ca4f2f",
+    "url": "/agoraDemo/static/js/2.4c81b543.chunk.js"
+  },
+  {
+    "revision": "2a4fce5ae2260c0fee84",
+    "url": "/agoraDemo/static/js/main.c7e5c76d.chunk.js"
+  },
+  {
+    "revision": "1fa8a486006bab74c775",
+    "url": "/agoraDemo/static/js/runtime~main.91e1782f.js"
+  },
+  {
+    "revision": "2ab67eec1a3ab14d3cc6fc3eaa072181",
+    "url": "/agoraDemo/static/media/ag-index-background.2ab67eec.png"
+  },
+  {
+    "revision": "c9b3e104479e2fab5588080e81dfb689",
+    "url": "/agoraDemo/static/media/ag-logo.c9b3e104.png"
+  },
+  {
+    "revision": "8504fae6a8575e0a9786f1c67bd05e69",
+    "url": "/agoraDemo/static/media/icons.8504fae6.eot"
+  },
+  {
+    "revision": "bc005709a21d5a2af39b0744935ace95",
+    "url": "/agoraDemo/static/media/icons.bc005709.woff"
+  },
+  {
+    "revision": "efe053d475acd04c59283fc07f718371",
+    "url": "/agoraDemo/static/media/icons.efe053d4.ttf"
+  }
+]);
